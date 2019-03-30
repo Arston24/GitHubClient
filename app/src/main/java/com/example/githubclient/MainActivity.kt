32e752203593c24802df8dd3fd599758ac6a2836
arsto <arston_sv@outlook.com>
2019-03-30 22:34:15 +0500

    repo list

---
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.githubclient
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.coroutines.CoroutineContext
+
+class MainActivity: AppCompatActivity(), CoroutineScope{
+
+    private val adapter = RepoAdapter()
+
+    private val rootJob = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + rootJob
+    private val httpClient = OkHttpClient.Builder()
+        .build()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+        val repoList: RecyclerView = findViewById(R.id.repoList)
+        repoList.layoutManager = LinearLayoutManager(this)
+        repoList.adapter = adapter
+        loadData()
+    }
+
+    private fun loadData() = launch {
+        val request = Request.Builder()
+            .url("https://api.github.com/orgs/google/repos")
+            .build()
+
+        val response: String = withContext(Dispatchers.IO){
+            httpClient.newCall(request).execute().body()!!.string()
+        }
+
+        val type = object: TypeToken<ArrayList<Repo>>(){}
+        val repos = Gson().fromJson<ArrayList<Repo>>(response, type.type)
+        adapter.data.clear()
+        adapter.data.addAll(repos)
+        adapter.notifyDataSetChanged()
+
+    }
+
+    override fun onDestroy() {
+        rootJob.cancel()
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/example/githubclient/Repo.kt
@@ -0,0 +1,3 @@
+package com.example.githubclient
+
+class Repo(val name: String)
